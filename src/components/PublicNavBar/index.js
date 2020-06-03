445904279a2784919d0207878cf9a6ed051b0ea5
Orim Dominic <orimdominic@gmail.com>
2020-06-02 22:00:01 -0400

    test: modify test to fit homepage

--- src/components/PublicNavBar/index.js
+++ src/components/PublicNavBar/index.js
@@ -1,41 +1,43 @@
 import React from 'react';
-import Button from '../Button'
+import Button from '../Button';
 import './index.css';
 import { Link } from 'react-router-dom';
 
 export default () => (
-<nav >
-<ul className="brand-container">
-  <li>
-    <Link to="/"><h2>Farmhub</h2></Link>
-  </li>
-</ul>
-<ul className="links-container" >
-  <li >
-    <ul>
+  <nav>
+    <ul className="brand-container">
       <li>
-        <Link to="/">Market</Link>
-      </li>
-      <li>
-        <Link to="/login">About</Link>
-      </li>
-      <li>
-        <Link to="/login">Contact</Link>
-      </li>
-    </ul>
-  </li>
-  <li >
-    <ul >
-      <li >
-        <Link  to="/login">
-          <Button>Sign in</Button>
+        <Link to="/">
+          <h2>Farmhub</h2>
         </Link>
       </li>
+    </ul>
+    <ul className="links-container">
       <li>
-          <Button>Sign up</Button>
+        <ul>
+          <li>
+            <Link to="/">Market</Link>
+          </li>
+          <li>
+            <Link to="/login">About</Link>
+          </li>
+          <li>
+            <Link to="/login">Contact</Link>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <ul>
+          <li>
+            <Link to="/login">
+              <Button>Sign in</Button>
+            </Link>
+          </li>
+          <li>
+            <Button>Sign up</Button>
+          </li>
+        </ul>
       </li>
     </ul>
-  </li>
-</ul>
-</nav>
-)
+  </nav>
+);
--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import './index.css'
-import headerImg from '../../assets/happy-farmer.jpg'
-import Header from "../../components/Header"
+import './index.css';
+import headerImg from '../../assets/happy-farmer.jpg';
+import Header from '../../components/Header';
 import PublicNavBar from '../../components/PublicNavBar';
 
 export default () => (
   <div style={{ width: '100%' }}>
-    <PublicNavBar></PublicNavBar>
+    <PublicNavBar />
     <Header>
-      <img src={headerImg} alt="a happy farmer"/>
+      <img src={headerImg} alt="a happy farmer" />
       <h1>The No.1 online platform for connecting farmers and buyers</h1>
     </Header>
   </div>
