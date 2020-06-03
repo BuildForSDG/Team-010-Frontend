1576eda4d1dca6bd154d485843ab1b4c4ed8c74a
Orim Dominic <orimdominic@gmail.com>
2020-06-02 21:22:35 -0400

    feat: incomplete homepage

---
+++ src/components/Header/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Header } from './styles';
+
+export default ({ children, ...props }) => <Header style={props.style}>{children}</Header>;
---
+++ src/components/PublicNavBar/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Button from '../Button'
+import './index.css';
+import { Link } from 'react-router-dom';
+
+export default () => (
+<nav >
+<ul className="brand-container">
+  <li>
+    <Link to="/"><h2>Farmhub</h2></Link>
+  </li>
+</ul>
+<ul className="links-container" >
+  <li >
+    <ul>
+      <li>
+        <Link to="/">Market</Link>
+      </li>
+      <li>
+        <Link to="/login">About</Link>
+      </li>
+      <li>
+        <Link to="/login">Contact</Link>
+      </li>
+    </ul>
+  </li>
+  <li >
+    <ul >
+      <li >
+        <Link  to="/login">
+          <Button>Sign in</Button>
+        </Link>
+      </li>
+      <li>
+          <Button>Sign up</Button>
+      </li>
+    </ul>
+  </li>
+</ul>
+</nav>
+)
